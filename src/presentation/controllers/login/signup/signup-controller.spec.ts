0b338b2a50227a9bc8e14c389f6b59e308e4d66c
Leonardo Figueiredo <leommko@gmail.com>
2020-08-14 00:23:50 -0300

    test: ensure SignUpController calls AddAccount with correct values

---
+++ src/presentation/controllers/login/signup/signup-controller.spec.ts
@@ -0,0 +1,55 @@
+import { SignUpController } from './signup-controller'
+import { AccountModel, AddAccount, AddAccountModel } from './signup-controller-protocols'
+import { HttpRequest } from '../../../protocols'
+
+const makeAddAccount = (): AddAccount => {
+  class AddAccountStub implements AddAccount {
+    async add (account: AddAccountModel): Promise<AccountModel> {
+      return await new Promise(resolve => resolve(makeFakeAccount()))
+    }
+  }
+  return new AddAccountStub()
+}
+
+const makeFakeAccount = (): AccountModel => ({
+  id: 'valid_id',
+  name: 'valid_name',
+  email: 'valid_email@mail.com',
+  password: 'valid_password'
+})
+
+const makeFakeRequest = (): HttpRequest => ({
+  body: {
+    name: 'any_name',
+    email: 'any_email@mail.com',
+    password: 'any_password',
+    passwordConfirmation: 'any_password'
+  }
+})
+
+interface SutTypes {
+  sut: SignUpController
+  addAccountStub: AddAccount
+}
+
+const makeSut = (): SutTypes => {
+  const addAccountStub = makeAddAccount()
+  const sut = new SignUpController(addAccountStub)
+  return {
+    sut,
+    addAccountStub
+  }
+}
+
+describe('SignUp Controller', () => {
+  test('Should call AddAccount with correct values', async () => {
+    const { sut, addAccountStub } = makeSut()
+    const addSpy = jest.spyOn(addAccountStub, 'add')
+    await sut.handle(makeFakeRequest())
+    expect(addSpy).toHaveBeenCalledWith({
+      name: 'any_name',
+      email: 'any_email@mail.com',
+      password: 'any_password'
+    })
+  })
+})
