afc58d1887193cb34ad101c2ae60b719e142432b
Leonardo Figueiredo <leommko@gmail.com>
2020-08-14 11:37:37 -0300

    feat: add DbLoadAccountByToken implementation

---
+++ src/data/usecases/account/load-account-by-token/db-load-account-by-token-protocols.ts
@@ -0,0 +1,4 @@
+export * from '../../../../domain/usecases/account/load-account-by-token'
+export * from '../../../../domain/models/account'
+export * from '../../../protocols/criptography/decrypter'
+export * from '../../../protocols/db/account/load-account-by-token-repository'
---
+++ src/data/usecases/account/load-account-by-token/db-load-account-by-token.ts
@@ -0,0 +1,19 @@
+import { LoadAccountByToken, AccountModel, Decrypter, LoadAccountByTokenRepository } from './db-load-account-by-token-protocols'
+
+export class DbLoadAccountByToken implements LoadAccountByToken {
+  constructor (
+    private readonly decrypter: Decrypter,
+    private readonly loadAccountByTokenRepository: LoadAccountByTokenRepository
+  ) {}
+
+  async load (accessToken: string): Promise<AccountModel> {
+    const token = await this.decrypter.decrypt(accessToken)
+    if (token) {
+      const account = await this.loadAccountByTokenRepository.loadByToken(accessToken)
+      if (account) {
+        return account
+      }
+    }
+    return null
+  }
+}
