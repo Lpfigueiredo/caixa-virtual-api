b56ff9ee721f54e742c058cae54f147b42226314
Leonardo Figueiredo <leommko@gmail.com>
2020-08-14 16:51:16 -0300

    feat: ensure AddCategoryController returns 500 if AddCategory throws

--- src/presentation/controllers/category/add-category/add-category-controller.ts
+++ src/presentation/controllers/category/add-category/add-category-controller.ts
@@ -1,16 +1,21 @@
 import { Controller, HttpRequest, HttpResponse } from '../../../protocols'
 import { AddCategory } from '../../../../domain/usecases/category/add-category'
+import { serverError } from '../../../helpers/http/http-helper'
 
 export class AddCategoryController implements Controller {
   constructor (private readonly addCategory: AddCategory) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const { accountId } = httpRequest
-    const { name } = httpRequest.body
-    await this.addCategory.add({
-      accountId,
-      name
-    })
-    return Promise.resolve(null)
+    try {
+      const { accountId } = httpRequest
+      const { name } = httpRequest.body
+      await this.addCategory.add({
+        accountId,
+        name
+      })
+      return Promise.resolve(null)
+    } catch (error) {
+      return serverError(error)
+    }
   }
 }
