0efaf0070515bc04eca8d6329ae7dbf35025cd31
Leonardo Figueiredo <leommko@gmail.com>
2020-08-14 07:00:02 -0300

    test: ensure AccountMongoRepo updates the account accessToken on updateAccessToken success

--- src/infra/db/mongodb/account/account-mongo-repository.spec.ts
+++ src/infra/db/mongodb/account/account-mongo-repository.spec.ts
@@ -59,4 +59,19 @@ describe('Account Mongo Repository', () => {
       expect(account).toBeFalsy()
     })
   })
+  describe('updateAccessToken()', () => {
+    test('Should update the account accessToken on updateAccessToken success', async () => {
+      const sut = makeSut()
+      const res = (await accountCollection.insertOne({
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password: 'any_password'
+      })).ops[0]
+      expect(res.accessToken).toBeFalsy()
+      await sut.updateAccessToken(res._id, 'any_token')
+      const account = await accountCollection.findOne({ _id: res._id })
+      expect(account).toBeTruthy()
+      expect(account.accessToken).toBe('any_token')
+    })
+  })
 })
