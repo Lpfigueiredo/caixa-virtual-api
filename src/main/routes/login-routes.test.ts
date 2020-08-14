e28ec6a5cad8638c6b8e75c062d33a8bd98e2595
Leonardo Figueiredo <leommko@gmail.com>
2020-08-14 07:47:57 -0300

    test: ensure sign up route returns 200 on signup success

---
+++ src/main/routes/login-routes.test.ts
@@ -0,0 +1,35 @@
+import request from 'supertest'
+import app from '../config/app'
+import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
+import { Collection } from 'mongodb'
+
+let accountCollection: Collection
+
+describe('Login Routes', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    accountCollection = await MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
+  })
+
+  describe('POST /signup', () => {
+    test('Should return 200 on signup success', async () => {
+      await request(app)
+        .post('/api/signup')
+        .send({
+          name: 'Leonardo',
+          email: 'leommko@gmail.com',
+          password: '123',
+          passwordConfirmation: '123'
+        })
+        .expect(200)
+    })
+  })
+})
