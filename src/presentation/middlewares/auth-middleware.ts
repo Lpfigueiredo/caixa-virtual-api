2d078bf1899f16b3e7489331f4102a1936fefc1b
Leonardo Figueiredo <leommko@gmail.com>
2020-08-14 11:40:25 -0300

    feat: add AuthMiddleware implementation

---
+++ src/presentation/middlewares/auth-middleware.ts
@@ -0,0 +1,24 @@
+import { Middleware, HttpRequest, HttpResponse, LoadAccountByToken } from './auth-middleware-protocols'
+import { forbidden, ok, serverError } from '../helpers/http/http-helper'
+import { AccessDeniedError } from '../errors'
+
+export class AuthMiddleware implements Middleware {
+  constructor (
+    private readonly loadAccountByToken: LoadAccountByToken
+  ) {}
+
+  async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+    try {
+      const accessToken = httpRequest.headers?.['x-access-token']
+      if (accessToken) {
+        const account = await this.loadAccountByToken.load(accessToken)
+        if (account) {
+          return ok({ accountId: account.id })
+        }
+      }
+      return forbidden(new AccessDeniedError())
+    } catch (error) {
+      return serverError(error)
+    }
+  }
+}
