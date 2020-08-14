f30de06486b02fd968a3cb0b8b01c56d4a8377db
Leonardo Figueiredo <leommko@gmail.com>
2020-08-14 20:08:43 -0300

    feat: ensure LoadCategoriesController returns 204 if LoadSurveys returns empty

--- src/presentation/controllers/category/load-categories/load-categories-controller.ts
+++ src/presentation/controllers/category/load-categories/load-categories-controller.ts
@@ -1,11 +1,11 @@
 import { Controller, HttpRequest, HttpResponse, LoadCategories } from './load-categories-controller-protocols'
-import { ok } from '../../../helpers/http/http-helper'
+import { ok, noContent } from '../../../helpers/http/http-helper'
 
 export class LoadCategoriesController implements Controller {
   constructor (private readonly loadCategories: LoadCategories) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     const categories = await this.loadCategories.load()
-    return ok(categories)
+    return categories.length ? ok(categories) : noContent()
   }
 }
