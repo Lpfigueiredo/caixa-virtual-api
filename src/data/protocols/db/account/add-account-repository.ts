3f7bdbc2bb6006de77081c58b568fd4622ba3c01
Leonardo Figueiredo <leommko@gmail.com>
2020-08-14 01:33:13 -0300

    feat: add AddAccountRepository protocol

---
+++ src/data/protocols/db/account/add-account-repository.ts
@@ -0,0 +1,6 @@
+import { AddAccountModel } from '../../../../domain/usecases/account/add-account'
+import { AccountModel } from '../../../../domain/models/account'
+
+export interface AddAccountRepository {
+  add (accountData: AddAccountModel): Promise<AccountModel>
+}
