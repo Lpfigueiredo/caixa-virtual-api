f8c4194440b5df47a5d760be51f878f2415f7ee6
Leonardo Figueiredo <leommko@gmail.com>
2020-08-14 20:01:45 -0300

    feat: ensure LoadCategoriesController returns 200 on success

--- src/presentation/controllers/category/load-categories/load-categories-controller.ts
+++ src/presentation/controllers/category/load-categories/load-categories-controller.ts
@@ -1,10 +1,11 @@
 import { Controller, HttpRequest, HttpResponse, LoadCategories } from './load-categories-controller-protocols'
+import { ok } from '../../../helpers/http/http-helper'
 
 export class LoadCategoriesController implements Controller {
   constructor (private readonly loadCategories: LoadCategories) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    await this.loadCategories.load()
-    return null
+    const categories = await this.loadCategories.load()
+    return ok(categories)
   }
 }
