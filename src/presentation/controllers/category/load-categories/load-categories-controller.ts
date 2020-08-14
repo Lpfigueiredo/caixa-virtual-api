f5380a15cd5f65b45037f5d28d809fdc71182478
Leonardo Figueiredo <leommko@gmail.com>
2020-08-14 20:11:27 -0300

    feat: ensure LoadCategoriesController returns 500 if LoadSurveys throws

--- src/presentation/controllers/category/load-categories/load-categories-controller.ts
+++ src/presentation/controllers/category/load-categories/load-categories-controller.ts
@@ -1,11 +1,15 @@
 import { Controller, HttpRequest, HttpResponse, LoadCategories } from './load-categories-controller-protocols'
-import { ok, noContent } from '../../../helpers/http/http-helper'
+import { ok, noContent, serverError } from '../../../helpers/http/http-helper'
 
 export class LoadCategoriesController implements Controller {
   constructor (private readonly loadCategories: LoadCategories) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const categories = await this.loadCategories.load()
-    return categories.length ? ok(categories) : noContent()
+    try {
+      const categories = await this.loadCategories.load()
+      return categories.length ? ok(categories) : noContent()
+    } catch (error) {
+      return serverError(error)
+    }
   }
 }
