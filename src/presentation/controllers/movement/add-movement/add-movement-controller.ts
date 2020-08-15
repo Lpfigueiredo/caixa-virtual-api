be2e6e6f769babf0fa93c4211ebce2107a70286d
Leonardo Figueiredo <leommko@gmail.com>
2020-08-15 17:17:23 -0300

    feat: ensure AddMovementController returns 500 if LoadCategoriesByAccountId throws

--- src/presentation/controllers/movement/add-movement/add-movement-controller.ts
+++ src/presentation/controllers/movement/add-movement/add-movement-controller.ts
@@ -1,17 +1,21 @@
 import { Controller, HttpRequest, HttpResponse } from '../../../protocols'
 import { LoadCategoriesByAccountId } from '../../../../domain/usecases/category/load-categories-by-account-id'
-import { forbidden } from '../../../helpers/http/http-helper'
+import { forbidden, serverError } from '../../../helpers/http/http-helper'
 import { InvalidParamError } from '../../../errors/invalid-param-error'
 
 export class AddMovementController implements Controller {
   constructor (private readonly loadByAccountId: LoadCategoriesByAccountId) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const { accountId } = httpRequest
-    const categories = await this.loadByAccountId.loadById(accountId)
-    if (!categories.length) {
-      return forbidden(new InvalidParamError('categoryId'))
+    try {
+      const { accountId } = httpRequest
+      const categories = await this.loadByAccountId.loadById(accountId)
+      if (!categories.length) {
+        return forbidden(new InvalidParamError('categoryId'))
+      }
+      return null
+    } catch (error) {
+      return serverError(error)
     }
-    return null
   }
 }
