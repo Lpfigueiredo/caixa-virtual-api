28109c34831069808f0682dcdcb73219cbc08ec2
Leonardo Figueiredo <leommko@gmail.com>
2020-08-14 22:55:16 -0300

    feat: ensure LoadCategoryController returns 204 if LoadCategoriesByAccountId returns empty

--- src/presentation/controllers/category/load-category-by-account-id/load-category-by-account-id-controller.ts
+++ src/presentation/controllers/category/load-category-by-account-id/load-category-by-account-id-controller.ts
@@ -1,6 +1,6 @@
 import { Controller, HttpRequest, HttpResponse } from '../../../protocols'
 import { LoadCategoriesByAccountId } from '../../../../domain/usecases/category/load-categories-by-account-id'
-import { ok } from '../../../helpers/http/http-helper'
+import { ok, noContent } from '../../../helpers/http/http-helper'
 
 export class LoadCategoryController implements Controller {
   constructor (private readonly loadCategoriesByAccountId: LoadCategoriesByAccountId) {}
@@ -8,6 +8,6 @@ export class LoadCategoryController implements Controller {
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     const { accountId } = httpRequest
     const categories = await this.loadCategoriesByAccountId.loadById(accountId)
-    return ok(categories)
+    return categories.length ? ok(categories) : noContent()
   }
 }
