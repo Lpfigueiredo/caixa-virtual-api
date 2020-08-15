a975ac9d46b4bdc0b30ff08b5e30a9f73e3d8de7
Leonardo Figueiredo <leommko@gmail.com>
2020-08-14 22:48:20 -0300

    test: ensure LoadCategoryController calls LoadCategoriesByAccountId with correct value

---
+++ src/presentation/controllers/category/load-category-by-account-id/load-category-by-account-id-controller.spec.ts
@@ -0,0 +1,53 @@
+import { HttpRequest } from '../../../protocols'
+import { CategoryModel } from '../../../../domain/models/category'
+import { LoadCategoriesByAccountId } from '../../../../domain/usecases/category/load-categories-by-account-id'
+import { LoadCategoryController } from './load-category-by-account-id-controller'
+
+const makeFakeRequest = (): HttpRequest => ({
+  accountId: 'any_account_id'
+})
+
+const makeFakeCategoryResult = (): CategoryModel[] => ([
+  {
+    id: 'any_id',
+    accountId: 'any_accountId',
+    name: 'any_name'
+  },
+  {
+    id: 'any_id',
+    accountId: 'any_accountId',
+    name: 'any_name'
+  }
+])
+
+const makeLoadCategoryByAccountId = (): LoadCategoriesByAccountId => {
+  class LoadCategoriesByAccountIdStub implements LoadCategoriesByAccountId {
+    async loadById (id: string): Promise<CategoryModel[]> {
+      return new Promise(resolve => resolve(makeFakeCategoryResult()))
+    }
+  }
+  return new LoadCategoriesByAccountIdStub()
+}
+
+interface SutTypes {
+  sut: LoadCategoryController
+  loadCategoriesByAccountIdStub: LoadCategoriesByAccountId
+}
+
+const makeSut = (): SutTypes => {
+  const loadCategoriesByAccountIdStub = makeLoadCategoryByAccountId()
+  const sut = new LoadCategoryController(loadCategoriesByAccountIdStub)
+  return {
+    sut,
+    loadCategoriesByAccountIdStub
+  }
+}
+
+describe('LoadCategory Controller', () => {
+  test('Should call LoadCategoriesByAccountId with correct value', async () => {
+    const { sut, loadCategoriesByAccountIdStub } = makeSut()
+    const loadByIdSpy = jest.spyOn(loadCategoriesByAccountIdStub, 'loadById')
+    await sut.handle(makeFakeRequest())
+    expect(loadByIdSpy).toHaveBeenCalledWith('any_account_id')
+  })
+})
