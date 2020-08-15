116865fc035390af932f2171cc46987f7d9554c4
Leonardo Figueiredo <leommko@gmail.com>
2020-08-15 20:24:12 -0300

    feat: ensure AccountMongoRepo updates the account totalBalance on updateTotalBalance success

--- src/infra/db/mongodb/account/account-mongo-repository.ts
+++ src/infra/db/mongodb/account/account-mongo-repository.ts
@@ -3,8 +3,10 @@ import { AddAccountModel } from '../../../../domain/usecases/account/add-account
 import { AccountModel } from '../../../../domain/models/account'
 import { MongoHelper } from '../helpers/mongo-helper'
 import { UpdateAccessTokenRepository } from '../../../../data/protocols/db/account/update-access-token-repository'
+import { UpdateAccountRepository } from '../../../../data/protocols/db/account/update-account-repository'
+import { ObjectId } from 'mongodb'
 
-export class AccountMongoRepository implements AddAccountRepository, UpdateAccessTokenRepository {
+export class AccountMongoRepository implements AddAccountRepository, UpdateAccessTokenRepository, UpdateAccountRepository {
   async add (accountData: AddAccountModel): Promise<AccountModel> {
     const accountCollection = await MongoHelper.getCollection('accounts')
     const newAccount = Object.assign({}, accountData, { totalBalance: 0 })
@@ -21,7 +23,7 @@ export class AccountMongoRepository implements AddAccountRepository, UpdateAcces
   async updateAccessToken (id: string, token: string): Promise<void> {
     const accountCollection = await MongoHelper.getCollection('accounts')
     await accountCollection.updateOne({
-      _id: id
+      _id: new ObjectId(id)
     }, {
       $set: {
         accessToken: token
@@ -33,5 +35,16 @@ export class AccountMongoRepository implements AddAccountRepository, UpdateAcces
     const accountCollection = await MongoHelper.getCollection('accounts')
     const account = await accountCollection.findOne({ accessToken: token })
     return account && MongoHelper.map(account)
+  }
+
+  async updateTotalBalance (id: string, value: number): Promise<void> {
+    const accountCollection = await MongoHelper.getCollection('accounts')
+    await accountCollection.updateOne({
+      _id: new ObjectId(id)
+    }, {
+      $inc: {
+        totalBalance: value
+      }
+    })
   }
 }
