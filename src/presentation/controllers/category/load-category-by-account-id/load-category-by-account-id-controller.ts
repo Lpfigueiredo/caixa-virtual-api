303ddb9e90d605839f228441a923f7e3fa539d2f
Leonardo Figueiredo <leommko@gmail.com>
2020-08-14 22:59:21 -0300

    feat: ensure LoadCategoryByAccountIdController returns 500 if LoadCategoriesByAccountId throws

--- src/presentation/controllers/category/load-category-by-account-id/load-category-by-account-id-controller.ts
+++ src/presentation/controllers/category/load-category-by-account-id/load-category-by-account-id-controller.ts
@@ -1,13 +1,17 @@
 import { Controller, HttpRequest, HttpResponse } from '../../../protocols'
 import { LoadCategoriesByAccountId } from '../../../../domain/usecases/category/load-categories-by-account-id'
-import { ok, noContent } from '../../../helpers/http/http-helper'
+import { ok, noContent, serverError } from '../../../helpers/http/http-helper'
 
 export class LoadCategoryController implements Controller {
   constructor (private readonly loadCategoriesByAccountId: LoadCategoriesByAccountId) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const { accountId } = httpRequest
-    const categories = await this.loadCategoriesByAccountId.loadById(accountId)
-    return categories.length ? ok(categories) : noContent()
+    try {
+      const { accountId } = httpRequest
+      const categories = await this.loadCategoriesByAccountId.loadById(accountId)
+      return categories.length ? ok(categories) : noContent()
+    } catch (error) {
+      return serverError(error)
+    }
   }
 }
