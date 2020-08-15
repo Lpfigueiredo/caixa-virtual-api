4af62b9feee5f6983fbf73281c8b60f10d996c34
Leonardo Figueiredo <leommko@gmail.com>
2020-08-15 17:10:07 -0300

    feat: ensure AddMovementController returns 403 if LoadCategoriesByAccountId returns empty

--- src/presentation/controllers/movement/add-movement/add-movement-controller.ts
+++ src/presentation/controllers/movement/add-movement/add-movement-controller.ts
@@ -1,12 +1,17 @@
 import { Controller, HttpRequest, HttpResponse } from '../../../protocols'
 import { LoadCategoriesByAccountId } from '../../../../domain/usecases/category/load-categories-by-account-id'
+import { forbidden } from '../../../helpers/http/http-helper'
+import { InvalidParamError } from '../../../errors/invalid-param-error'
 
 export class AddMovementController implements Controller {
   constructor (private readonly loadByAccountId: LoadCategoriesByAccountId) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     const { accountId } = httpRequest
-    await this.loadByAccountId.loadById(accountId)
+    const categories = await this.loadByAccountId.loadById(accountId)
+    if (!categories.length) {
+      return forbidden(new InvalidParamError('categoryId'))
+    }
     return null
   }
 }
