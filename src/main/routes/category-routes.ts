03976b3e1bdd30e153acfa06b340f0551c73b691
Leonardo Figueiredo <leommko@gmail.com>
2020-08-15 00:42:33 -0300

    feat: add load categories route

--- src/main/routes/category-routes.ts
+++ src/main/routes/category-routes.ts
@@ -3,7 +3,9 @@ import { adaptRoute } from '../adapters/express/express-route-adapter'
 import { makeAddCategoryController } from '../factories/controllers/category/add-category/add-category-controller-factory'
 import { auth } from '../middlewares/auth'
 import { addCategoryValidation } from '../middlewares/add-category-validation'
+import { makeLoadCategoryController } from '../factories/controllers/category/load-category/load-category-controller-factory'
 
 export default (router: Router): void => {
   router.post('/categories', addCategoryValidation, auth, adaptRoute(makeAddCategoryController()))
+  router.get('/categories', auth, adaptRoute(makeLoadCategoryController()))
 }
