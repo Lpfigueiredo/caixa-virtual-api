808e691f11cda26d7cf0c93186201b5ed7a6e58b
Leonardo Figueiredo <leommko@gmail.com>
2020-08-14 22:51:02 -0300

    feat: ensure LoadCategoryController returns 200 on success

--- src/presentation/controllers/category/load-category-by-account-id/load-category-by-account-id-controller.ts
+++ src/presentation/controllers/category/load-category-by-account-id/load-category-by-account-id-controller.ts
@@ -1,12 +1,13 @@
 import { Controller, HttpRequest, HttpResponse } from '../../../protocols'
 import { LoadCategoriesByAccountId } from '../../../../domain/usecases/category/load-categories-by-account-id'
+import { ok } from '../../../helpers/http/http-helper'
 
 export class LoadCategoryController implements Controller {
   constructor (private readonly loadCategoriesByAccountId: LoadCategoriesByAccountId) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     const { accountId } = httpRequest
-    await this.loadCategoriesByAccountId.loadById(accountId)
-    return null
+    const categories = await this.loadCategoriesByAccountId.loadById(accountId)
+    return ok(categories)
   }
 }
