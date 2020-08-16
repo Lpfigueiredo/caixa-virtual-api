103518500fcaac7e83d32b29d854c54a90dd7e44
Leonardo Figueiredo <leommko@gmail.com>
2020-08-16 14:30:01 -0300

    feat: ensure LoadDailyMovementController returns 500 if LoadDailyMovement throws

--- src/presentation/controllers/daily-movement/load-daily-movement/load-daily-movement-controller.ts
+++ src/presentation/controllers/daily-movement/load-daily-movement/load-daily-movement-controller.ts
@@ -1,15 +1,20 @@
 import { Controller, HttpRequest, HttpResponse } from '../../../protocols'
 import { LoadDailyMovement } from '../../../../domain/usecases/daily-movement/load-daily-movement'
+import { serverError } from '../../../helpers/http/http-helper'
 
 export class LoadDailyMovementController implements Controller {
   constructor (private readonly loadDailyMovement: LoadDailyMovement) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const { accountId } = httpRequest
-    await this.loadDailyMovement.load({
-      accountId,
-      date: new Date()
-    })
-    return null
+    try {
+      const { accountId } = httpRequest
+      await this.loadDailyMovement.load({
+        accountId,
+        date: new Date()
+      })
+      return null
+    } catch (error) {
+      return serverError(error)
+    }
   }
 }
