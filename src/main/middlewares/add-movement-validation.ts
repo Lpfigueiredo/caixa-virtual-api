cb1e66b27d6fa37fd25d15eac79321a2bff1f923
Leonardo Figueiredo <leommko@gmail.com>
2020-08-17 03:43:09 -0300

    fix: change min value validation

--- src/main/middlewares/add-movement-validation.ts
+++ src/main/middlewares/add-movement-validation.ts
@@ -3,7 +3,7 @@ import { celebrate, Segments, Joi } from 'celebrate'
 export const addMovementValidation = celebrate({
   [Segments.BODY]: Joi.object().keys({
     categoryId: Joi.string().length(24).hex().required(),
-    value: Joi.number().min(0).precision(2).required(),
+    value: Joi.number().min(0.01).precision(2).required(),
     description: Joi.string().required()
   })
 })
