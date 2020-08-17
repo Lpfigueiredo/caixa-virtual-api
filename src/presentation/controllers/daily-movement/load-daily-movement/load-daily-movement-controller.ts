bfb3aad42f394bc088861f27791df1dd1be74678
Leonardo Figueiredo <leommko@gmail.com>
2020-08-16 23:07:18 -0300

    feat: ensure LoadDailyMovementController returns 204 if LoadDailyMovement returns null

--- src/presentation/controllers/daily-movement/load-daily-movement/load-daily-movement-controller.ts
+++ src/presentation/controllers/daily-movement/load-daily-movement/load-daily-movement-controller.ts
@@ -1,6 +1,6 @@
 import { Controller, HttpRequest, HttpResponse } from '../../../protocols'
 import { LoadDailyMovement } from '../../../../domain/usecases/daily-movement/load-daily-movement'
-import { serverError, ok } from '../../../helpers/http/http-helper'
+import { serverError, ok, noContent } from '../../../helpers/http/http-helper'
 
 export class LoadDailyMovementController implements Controller {
   constructor (private readonly loadDailyMovement: LoadDailyMovement) {}
@@ -12,7 +12,7 @@ export class LoadDailyMovementController implements Controller {
         accountId,
         date: new Date(new Date().setHours(0, 0, 0))
       })
-      return ok(dailyMovement)
+      return dailyMovement ? ok(dailyMovement) : noContent()
     } catch (error) {
       return serverError(error)
     }
