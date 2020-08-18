0c46c8735424e0b597897e365a43a5becfb9d5f0
Leonardo Figueiredo <leommko@gmail.com>
2020-08-17 21:15:39 -0300

    feat: add max length string criterion to validators

--- src/main/middlewares/add-category-validation.ts
+++ src/main/middlewares/add-category-validation.ts
@@ -2,6 +2,6 @@ import { celebrate, Segments, Joi } from 'celebrate'
 
 export const addCategoryValidation = celebrate({
   [Segments.BODY]: Joi.object().keys({
-    name: Joi.string().required()
+    name: Joi.string().max(32).required()
   })
 })
--- src/main/middlewares/add-movement-validation.ts
+++ src/main/middlewares/add-movement-validation.ts
@@ -3,7 +3,7 @@ import { celebrate, Segments, Joi } from 'celebrate'
 export const addMovementValidation = celebrate({
   [Segments.BODY]: Joi.object().keys({
     value: Joi.number().min(0.01).precision(2).required(),
-    description: Joi.string().required()
+    description: Joi.string().max(100).required()
   }),
   [Segments.PARAMS]: {
     categoryId: Joi.string().length(24).hex().required()
